d485e67a9d1ba922a2dd638b0269d314af947077
M Rawat (cyource) <manas.rawat2002@gmail.com>
2016-02-02 20:27:10 +0000

    Initial commit

---
+++ app/src/main/java/cyource/manasrawat/overfloat/FloatingButtonService.java
@@ -0,0 +1,85 @@
+package cyource.manasrawat.overfloat;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class FloatingButtonService extends Service {
+
+    private WindowManager windowManager;
+    private ImageView floatingButton;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        floatingButton = new ImageView(this);
+        floatingButton.setImageResource(R.mipmap.openfloat);
+        floatingButton.setLayoutParams(new LinearLayout.LayoutParams(5, 5));
+
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(floatingButton, params);
+
+        floatingButton.setOnTouchListener(new View.OnTouchListener() {
+            int x;
+            int y;
+            float touchX;
+            float touchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        x = params.x;
+                        y = params.y;
+                        touchX = event.getRawX();
+                        touchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        startActivity(getPackageManager().getLaunchIntentForPackage("cyource.manasrawat.cycripter"));
+                        //stopService(new Intent(getApplicationContext(), FloatingButtonService.class));
+                        return false;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = x + (int) (event.getRawX() - touchX);
+                        params.y = y + (int) (event.getRawY() - touchY);
+                        windowManager.updateViewLayout(floatingButton, params);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (floatingButton != null) {
+            windowManager.removeView(floatingButton);
+        }
+    }
+}
+
