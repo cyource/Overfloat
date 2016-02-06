2af538db1d2c880088aa66059ce9cf15dab991df
M Rawat (cyource) <manas.rawat2002@gmail.com>
2016-02-06 21:24:38 +0000

    App Icon, Animations added, App-List Alphabetical Ordering, UI Changes and Code Syntax Simplification

--- app/src/main/java/cyource/manasrawat/overfloat/Overfloat.java
+++ app/src/main/java/cyource/manasrawat/overfloat/Overfloat.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Overfloat extends AppCompatActivity {
@@ -21,11 +23,20 @@ public class Overfloat extends AppCompatActivity {
         setContentView(R.layout.overfloat);
         setSupportActionBar((android.support.v7.widget.Toolbar) findViewById(R.id.toolbar));
         List<PackageInfo> appsList = getPackageManager().getInstalledPackages(PackageManager.GET_META_DATA);
+        Collections.sort(appsList, new Comparator<PackageInfo>() {
+            @Override
+            public int compare(PackageInfo lhs, PackageInfo rhs) {
+                return lhs.applicationInfo.loadLabel(getPackageManager()).toString().compareTo
+                        (rhs.applicationInfo.loadLabel(getPackageManager()).toString());
+            }
+        });
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
         for (int i = 0; i < appsList.size(); i++) {
             PackageInfo packageInfo = appsList.get(i);
             String appName = packageInfo.applicationInfo.loadLabel(getPackageManager()).toString();
             final String packageName = packageInfo.packageName;
-            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
             final TextView textView = new TextView(this);
             textView.setText(appName);
             textView.setTextSize(18);
@@ -34,10 +45,7 @@ public class Overfloat extends AppCompatActivity {
             getApplication().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, typedValue, true);
             textView.setBackgroundResource(typedValue.resourceId);
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 50);
-            int bottom = 0;
-            if (i == appsList.size() - 1)
-                bottom = 40;
-            layoutParams.setMargins(40, 40, 40, bottom);
+            layoutParams.setMargins(40, 40, 40, 0);
             if (getPackageManager().getLaunchIntentForPackage(packageName) != null)
                 linearLayout.addView(textView, layoutParams);
             final Intent intent = new Intent(getApplicationContext(), FloatingButtonService.class);
@@ -50,5 +58,8 @@ public class Overfloat extends AppCompatActivity {
                 }
             });
         }
+        View view = new View(this);
+        LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 40);
+        linearLayout.addView(view, layoutParams2);
     }
 }
