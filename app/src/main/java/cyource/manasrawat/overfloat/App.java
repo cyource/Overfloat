a8785db9840b5fbd5d33d34c387bd3f1acf65c9d
M Rawat (cyource) <manas.rawat2002@gmail.com>
2016-02-16 17:41:39 +0000

    Major Update: Multiple app adding capability to floating button-menu and more cool stuff.

---
+++ app/src/main/java/cyource/manasrawat/overfloat/App.java
@@ -0,0 +1,116 @@
+package cyource.manasrawat.overfloat;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class App extends AppCompatActivity {
+
+    ArrayList<String> packageNames;
+    ArrayList<String> appNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        final List<PackageInfo> appsList = getPackageManager().getInstalledPackages(PackageManager.GET_META_DATA);
+        Collections.sort(appsList, new Comparator<PackageInfo>() {
+            @Override
+            public int compare(PackageInfo lhs, PackageInfo rhs) {
+                return lhs.applicationInfo.loadLabel(getPackageManager()).toString().compareTo
+                        (rhs.applicationInfo.loadLabel(getPackageManager()).toString());
+            }
+        });
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        packageNames = new ArrayList<>();
+        appNames = new ArrayList<>();
+        final Intent intent = new Intent(getApplicationContext(), Run.class);
+        TypedValue typedValue = new TypedValue();
+        getApplication().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, typedValue, true);
+
+        for (int i = 0; i < appsList.size(); i++) {
+            PackageInfo packageInfo = appsList.get(i);
+            final String appName = packageInfo.applicationInfo.loadLabel(getPackageManager()).toString();
+            final String packageName = packageInfo.packageName;
+            final TextView textView = new TextView(this);
+            textView.setText(appName);
+            textView.setTextSize(18);
+            textView.setClickable(true);
+            textView.setBackgroundResource(typedValue.resourceId);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 50);
+            layoutParams.setMargins(40, 40, 40, 0);
+            RelativeLayout relativeLayout = new RelativeLayout(this);
+            relativeLayout.setClickable(true);
+            relativeLayout.setBackgroundResource(typedValue.resourceId);
+            RelativeLayout.LayoutParams relativeLayoutLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 50);
+            relativeLayoutLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            if (getPackageManager().getLaunchIntentForPackage(packageName) != null) {
+                relativeLayout.addView(textView);
+                final CheckBox checkBox = new CheckBox(this);
+                checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (checkBox.isChecked()) {
+                            packageNames.add(packageName);
+                            appNames.add(appName);
+                        } else {
+                            packageNames.remove(packageName);
+                            appNames.remove(appName);
+                        }
+                    }
+                });
+                onClick(relativeLayout, checkBox);
+                onClick(textView, checkBox);
+                relativeLayout.addView(checkBox, relativeLayoutLayoutParams);
+                linearLayout.addView(relativeLayout, layoutParams);
+            }
+        }
+        View view = new View(this);
+        LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 40);
+        linearLayout.addView(view, layoutParams2);
+
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+                startService(intent
+                        .putStringArrayListExtra("PACKAGE_NAMES", packageNames)
+                        .putStringArrayListExtra("APP_NAMES", appNames));
+            }
+        });
+    }
+
+    public void onClick(View view, final CheckBox checkBox) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    checkBox.setChecked(false);
+                } else {
+                    checkBox.setChecked(true);
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/cyource/manasrawat/overfloat/Run.java
+++ app/src/main/java/cyource/manasrawat/overfloat/Run.java
@@ -2,25 +2,48 @@ package cyource.manasrawat.overfloat;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Run extends android.app.Service {
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringConfig;
+import com.facebook.rebound.SpringSystem;
+import com.facebook.rebound.SpringUtil;
+
+import java.util.ArrayList;
+
+public class Run extends Service {
 
     private WindowManager windowManager;
     private ImageView floatingButton;
+    private LinearLayout linearLayout;
+    private ImageView removeView;
+    private float scale;
+    private Spring spring;
+    private WindowManager.LayoutParams params;
+    private int i;
+    private ArrayList<String> packageNames;
+    private ArrayList<String> appNames;
+
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
 
     @Override
     public void onCreate() {
@@ -28,84 +51,241 @@ public class Run extends android.app.Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, final int startId) {
-        final String packageName = intent.getStringExtra("PACKAGE_NAME");
+    public int onStartCommand(Intent intent, final int flags, final int startId) {
+        packageNames = intent.getStringArrayListExtra("PACKAGE_NAMES");
+        appNames = intent.getStringArrayListExtra("APP_NAMES");
+        final int[] position = {0, -20};
+        TypedValue typedValue = new TypedValue();
+        getApplication().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, typedValue, true);
+
+        // (VIEW DECLARATIONS) {
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         floatingButton = new ImageView(this);
         floatingButton.setX(-100);
         floatingButton.animate().translationX(0);
         floatingButton.setImageResource(R.mipmap.floatingbutton);
-        floatingButton.setLayoutParams(new LinearLayout.LayoutParams(5, 5));
 
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        removeView = new ImageView(this);
+        removeView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
+        removeView.setImageResource(R.drawable.ic_close_white_36dp);
+        removeView.setVisibility(View.GONE);
+        removeView.setPadding(20, 20, 20, 20);
+        removeView.setY(-100);
+        removeView.setBackgroundResource(typedValue.resourceId);
+        removeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                params.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+                params.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                stopSelf();
+            }
+        });
+
+        linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        if (appNames.isEmpty()){
+            TextView emptyView = new TextView(this);
+            emptyView.setTextSize(20);
+            emptyView.setPadding(20, 20, 20, 20);
+            emptyView.setText("No Apps Selected");
+            emptyView.setGravity(Gravity.CENTER_HORIZONTAL);
+            linearLayout.addView(emptyView);
+        }
+
+        for (i = 0; i < appNames.size(); i++) {
+            TextView textView = new TextView(this);
+            textView.setTextSize(20);
+            textView.setPadding(20, 20, 20, 20);
+            textView.setBackgroundResource(typedValue.resourceId);
+            textView.setGravity(Gravity.CENTER_HORIZONTAL);
+            final String a = packageNames.get(i);
+            textView.setText(appNames.get(i));
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(getPackageManager().getLaunchIntentForPackage(a));
+                    gone();
+                    resize(1f);
+                }
+            });
+
+            linearLayout.addView(textView);
+        }
+
+        linearLayout.setVisibility(View.GONE);
+        //}
+
+        // (WINDOWMANAGER PARAMS) {
+
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = position[0];
+        params.y = position[1];
+        params.dimAmount = 0.6f;
 
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = 100;
+        WindowManager.LayoutParams params2 = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params2.gravity = Gravity.CENTER;
 
-        windowManager.addView(floatingButton, params);
+        WindowManager.LayoutParams params3 = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, 150,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params3.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        //}
 
-        floatingButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                floatingButton.setOnLongClickListener(null);
-                floatingButton.setOnTouchListener(null);
-                stopSelf();
-                return true;
-            }
-        });
+        // (REBOUND SPRING COMPONENTS + ONTOUCH LISTENER) {
+        SpringSystem springSystem = SpringSystem.create();
+        spring = springSystem.createSpring()
+                .setSpringConfig(new SpringConfig(180, 20))
+                .setCurrentValue(1.0)
+                .addListener(new SimpleSpringListener() {
+                    @Override
+                    public void onSpringUpdate(Spring spring) {
+                        params.x = (int) (SpringUtil.mapValueFromRangeToRange(spring.getCurrentValue()
+                                , 0, 1, 25, position[0]));
+                        params.y = (int) (SpringUtil.mapValueFromRangeToRange(spring.getCurrentValue()
+                                , 0, 1, 25, position[1]));
+                        windowManager.updateViewLayout(floatingButton, params);
+                    }
+                });
 
         floatingButton.setOnTouchListener(new View.OnTouchListener() {
+
+            //INTS + FLOATS
             int intX;
             int intY;
             float touchX;
             float touchY;
-            float distanceX;
+
+            //BOOLEANS
+            boolean motion = false;
+            boolean visible = false;
+            boolean drag = false;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        motion = false;
                         intX = params.x;
                         intY = params.y;
                         touchX = event.getRawX();
                         touchY = event.getRawY();
-                        distanceX = event.getX();
                         return false;
                     case MotionEvent.ACTION_UP:
-                        float distance = distanceX - event.getX();
-                        if (distance == 0)
-                            startActivity(getPackageManager().getLaunchIntentForPackage(packageName));
+                        if (motion)
+                            return true;
+                        if (!visible) {
+                            floatingButton.getLocationOnScreen(position);
+                            params.flags &= ~WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+                            params.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                            linearLayout.setVisibility(View.VISIBLE);
+                            removeView.setVisibility(View.VISIBLE);
+                            animation(0, true, true, null);
+                            drag = true;
+                            scale = 1f - (((float) spring.getCurrentValue()) * 0.2f);
+                            spring.setEndValue(0.0);
+                        } else {
+                            drag = false;
+                            scale = 1f;
+                            gone();
+                        }
+                        resize(scale);
+                        visible = !visible;
+                        windowManager.updateViewLayout(floatingButton, params);
                         return false;
                     case MotionEvent.ACTION_MOVE:
-                        params.x = intX + (int) (event.getRawX() - touchX);
-                        params.y = intY + (int) (event.getRawY() - touchY);
-                        windowManager.updateViewLayout(floatingButton, params);
-                        return true;
+                        if (!drag) {
+                            motion = true;
+                            params.x = intX + (int) (event.getRawX() - touchX);
+                            params.y = intY + (int) (event.getRawY() - touchY);
+                            windowManager.updateViewLayout(floatingButton, params);
+                            return true;
+                        }
+                        return false;
                 }
                 return false;
             }
         });
+        //}
+
+        //VIEWS ADDED TO WINDOWMANAGER
+        windowManager.addView(floatingButton, params);
+        windowManager.addView(linearLayout, params2);
+        windowManager.addView(removeView, params3);
+
         return START_STICKY;
     }
+
+    // (SECONDARY METHODS) {
+    public void gone() {
+        params.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        params.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        linearLayout.setVisibility(View.GONE);
+        animation(-100, true, true, null);
+        spring.setEndValue(1.0);
+    }
+
+    public void resize(final float scale) {
+        spring.addListener(new SimpleSpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                super.onSpringUpdate(spring);
+                floatingButton.setScaleX(scale);
+                floatingButton.setScaleY(scale);
+            }
+        });
+    }
+
+    public void animation(final int to, final boolean vis, boolean trans, final View view) {
+
+        if (trans) {
+
+            removeView.animate().translationY(to).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationCancel(animation);
+                    if (vis) {
+                        if (to == -100)
+                            removeView.setVisibility(View.GONE);
+                    } else {
+                        windowManager.removeView(removeView);
+                        removeView.setOnClickListener(null);
+                    }
+                }
+            });
+        } else {
+            view.animate().alpha(0.0f).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationCancel(animation);
+                    windowManager.removeView(view);
+                    view.setOnClickListener(null);
+                }
+            });
+
+        }
+    }
+    //}
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (floatingButton != null) {
-            floatingButton.animate().alpha(0.0f).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationCancel(Animator animation) {
-                    super.onAnimationCancel(animation);
-                    windowManager.removeView(floatingButton);
-                }
-            });
-        }
+        View[] list = {floatingButton, linearLayout};
+        for (int i = 0; i <= 1; i++)
+            animation(0, false, false, list[i]);
+        animation(-100, false, true, null);
     }
 }
-
